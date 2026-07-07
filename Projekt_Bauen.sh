6c24c564fc6b2715ceaff817342a768294997a38
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-07 16:24:03 +0300

    Refactor Projekt_Bauen.sh for improved clarity
    Updated build script to simplify directory navigation and build process.

--- Projekt_Bauen.sh
+++ Projekt_Bauen.sh
@@ -1,35 +1,19 @@
-#!/bin/sh
-set -e
+# Gehe in den Downloads-Ordner
+cd /Users/boyan1/Downloads
 
-# In das Verzeichnis des Skripts wechseln
-cd "$(dirname "$0")"
-
-# 1. Alles sauber zurücksetzen (verhindert alte Inkompatibilitäten)
-echo "Bereinige alte Build-Daten..."
+# Radikaler Löschvorgang (Alles weg!)
 rm -rf Lilu MacKernelSDK WhateverGreen
 
-# 2. Repositories klonen (Offizielle Acidanthera-Quellen)
-echo "Klone Repositories..."
+# Frisch klonen (Nur von offiziellen Quellen)
 git clone https://github.com/acidanthera/Lilu
 git clone https://github.com/acidanthera/MacKernelSDK
 git clone https://github.com/acidanthera/WhateverGreen
 
-# 3. Lilu bauen
-echo "Baue Lilu..."
+# Bauen in der richtigen Reihenfolge
 cd Lilu
 ln -s ../MacKernelSDK MacKernelSDK
 xcodebuild -configuration Debug
-cd ..
-
-# 4. WhateverGreen bauen
-echo "Baue WhateverGreen..."
-cd WhateverGreen
+cd ../WhateverGreen
 ln -s ../MacKernelSDK MacKernelSDK
 ln -s ../Lilu/build/Debug/Lilu.kext Lilu.kext
 xcodebuild -configuration Debug
-cd ..
-
-echo "------------------------------------------------"
-echo "Erfolgreich gebaut!"
-echo "Lilu: Lilu/build/Debug/Lilu.kext"
-echo "WhateverGreen: WhateverGreen/build/Debug/WhateverGreen.kext"
