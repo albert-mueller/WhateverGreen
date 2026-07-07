ece9f57fe21d54f440524ad8056572e8cc16b23e
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-07 16:28:50 +0300

    Refactor build script for Lilu and WhateverGreen
    Updated cloning sources and build process for Lilu and WhateverGreen.

--- Projekt_Bauen.sh
+++ Projekt_Bauen.sh
@@ -1,19 +1,30 @@
-# Gehe in den Downloads-Ordner
+#!/bin/sh
+set -e
+
 cd /Users/boyan1/Downloads
 
-# Radikaler Löschvorgang (Alles weg!)
+# 1. Radikal aufräumen
 rm -rf Lilu MacKernelSDK WhateverGreen
 
-# Frisch klonen (Nur von offiziellen Quellen)
-git clone https://github.com/albert-mueller/Lilu
+# 2. OFFIZIELLE Quellen klonen (Hier liegt die Lösung für deinen Build-Fehler)
+git clone https://github.com/acidanthera/Lilu
 git clone https://github.com/acidanthera/MacKernelSDK
-git clone https://github.com/albert-mueller/WhateverGreen
+git clone https://github.com/acidanthera/WhateverGreen
 
-# Bauen in der richtigen Reihenfolge
+# 3. Lilu bauen
 cd Lilu
 ln -s ../MacKernelSDK MacKernelSDK
-xcodebuild -configuration Debug
-cd ../WhateverGreen
+# Wir nutzen -project für maximale Stabilität
+xcodebuild -project Lilu.xcodeproj -configuration Debug
+cd ..
+
+# 4. WhateverGreen bauen
+cd WhateverGreen
 ln -s ../MacKernelSDK MacKernelSDK
+# Lilu als Symlink für WhateverGreen verknüpfen
 ln -s ../Lilu/build/Debug/Lilu.kext Lilu.kext
-xcodebuild -configuration Debug
+# Bauen
+xcodebuild -project WhateverGreen.xcodeproj -configuration Debug
+cd ..
+
+echo "Build erfolgreich abgeschlossen!"
